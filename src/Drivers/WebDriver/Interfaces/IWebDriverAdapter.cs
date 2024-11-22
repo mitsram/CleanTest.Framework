59740e8cb7e72dd39027d8d7c146fb0528075577
Mits Ramos <mitsram@gmail.com>
2024-11-23 09:43:02 +1300

    Updated

--- src/Drivers/WebDriver/Interfaces/IWebDriverAdapter.cs
+++ src/Drivers/WebDriver/Interfaces/IWebDriverAdapter.cs
@@ -2,16 +2,18 @@ namespace CleanTest.Framework.WebDriver.Interfaces;
 
 public interface IWebDriverAdapter
 {
-    void NavigateToUrl(string url);
     string GetCurrentUrl();
+    void NavigateToUrl(string url);    
     IWebElementAdapter FindElementById(string id);
-    IWebElementAdapter FindElementByXPath(string xpath);
-    IWebElementAdapter FindElementByClassName(string className);
-    IReadOnlyCollection<IWebElementAdapter> FindElementsByCssSelector(string cssSelector);
-    IReadOnlyCollection<IWebElementAdapter> FindElementsByXPath(string xpath);
+    IWebElementAdapter FindElementByClassName(string className);   
     IReadOnlyCollection<IWebElementAdapter> FindElementsByClassName(string className);
-    void Dispose();
-
+    IReadOnlyCollection<IWebElementAdapter> FindElementsByCssSelector(string cssSelector);
+    IWebElementAdapter FindElementByXPath(string xpath);
+    IReadOnlyCollection<IWebElementAdapter> FindElementsByXPath(string xpath);
+    IWebElementAdapter FindElementByPlaceholder(string placeholder);
+    IWebElementAdapter FindElementByRole(string role);
+    IWebElementAdapter FindElementByLabel(string label);
     IWebElementAdapter WaitAndFindElementByXPath(string xpath, int timeoutInSeconds = 15);
+    void Dispose();
 }
 
--- src/Factories/ApiDriverFactory.cs
+++ src/Factories/ApiDriverFactory.cs
@@ -6,20 +6,20 @@ namespace CleanTest.Framework.Factories;
 public static class ApiDriverFactory
 {
     /// <summary>
-    /// Creates an instance of an API driver adapter based on the specified base URL and optional authenticator.
+    /// Creates an instance of an API driver adapter based on the specified base URL and optional client type.
+    /// If the client type is null or empty, it defaults to "RestSharp".
     /// </summary>
     /// <param name="baseUrl">The base URL for the API.</param>
-    /// <param name="authenticator">An optional MicrosoftAuthenticator for authentication.</param>
-    /// <returns>An instance of IApiDriverAdapter.</returns>
-    public static IApiDriverAdapter Create(string baseUrl)
+    /// <param name="clientType">An optional string specifying the type of API client to create. Defaults to "RestSharp".</param>
+    /// <returns>An instance of <see cref="IApiDriverAdapter"/> corresponding to the specified client type.</returns>
+    /// <exception cref="ArgumentException">Thrown when the specified client type is unsupported.</exception>
+    public static IApiDriverAdapter Create(string baseUrl, string clientType = "RestSharp")
     {
-        string apiClientType = "RestSharp";
-        
-        return apiClientType.ToLower() switch
+        return clientType.ToLower() switch
         {
             "httpclient" => new HttpClientAdapter(baseUrl),
             "restsharp" => new RestSharpAdapter(baseUrl),
-            _ => throw new ArgumentException($"Unsupported API client type: {apiClientType}")
+            _ => throw new ArgumentException($"Unsupported API client type: {clientType}")
         };
     }
 }
